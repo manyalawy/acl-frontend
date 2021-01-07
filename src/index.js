2976d360fbf8af9d183900f4c80d0b5b0a5e8ff7
manyalawy <manyalawy@gmail.com>
2021-01-08 01:35:02 +0200

    view and update profile implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Login from "./Components/Login/login";
+import Login from "./Components/Pages/Staff Members/Login/Login";
+import ViewMyProfile from "./Components/Pages/Staff Members/View_my_profile/ViewMyProfile";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import HRNav from "./Components/NavBars/hrNav";
+import HRNav from "./Components/NavBars/hrNav";
 import HODNav from "./Components/NavBars/hodNav";
 
 ReactDOM.render(
   <Router>
     <HODNav />
     <Route path="/login" exact component={Login} />
+    <Route path="/" exact component={ViewMyProfile} />
   </Router>,
 
   document.getElementById("root")
