fd67d3408b7037cc38eb381ca8f7e90a664bf8bb
manyalawy <manyalawy@gmail.com>
2021-01-11 23:18:53 +0200

    Change day off page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import React from "react";
 import ReactDOM from "react-dom";
 import Login from "./Components/Pages/Staff Members/Login/Login";
@@ -10,17 +11,21 @@ import viewCoverage from "./Components/Pages/Academic Members/Course Instructor/
 import viewAllStaff from "./Components/Pages/Academic Members/Course Instructor/viewAllStaff";
 import CoursesListView from "./Components/List Views/coursesListView";
 import InstructorsListView from "./Components/List Views/InstructorsListViewPerCourse";
+// eslint-disable-next-line
 import isAuth from "./Services/isAuth";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// eslint-disable-next-line
 import HRNav from "./Components/NavBars/hrNav";
 import HODNav from "./Components/NavBars/hodNav";
 import { SlotLinkingRequests } from "./Components/Pages/Academic Members/Course Coordinator/SlotLinkingRequests";
 import { CourseSlots } from "./Components/Pages/Academic Members/Course Coordinator/CourseSlots";
+import SendDayOffRequest from "./Components/Pages/Academic Members/Academic Member/Change Day Off Request/ChangeDayOff";
 
 ReactDOM.render(
   <Router>
     <HODNav />
     <Route path="/login" exact component={Login} />
+    <Route path="/changeDayOff" exact component={SendDayOffRequest} />
     <Route path="/" exact component={ViewMyProfile} />
     <Route path="/resetPassword" exact component={ResetPassowrd} />
     <Route path="/attendance" exact component={ViewAttendance} />
