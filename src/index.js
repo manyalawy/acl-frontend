bab6f47ddcd99110d3825ad39a22e739335e6fc9
Heidi Talaat <heiditalaat95@gmail.com>
2021-01-14 20:40:52 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ import ReactDOM from "react-dom";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import ProtectedRoute from './Services/ProtectedRoute'
+
+//staff members
 import Login from "./Components/Pages/Staff Members/Login/Login";
 import ViewMyProfile from "./Components/Pages/Staff Members/View_my_profile/ViewMyProfile";
 import ResetPassowrd from "./Components/Pages/Staff Members/Reset password/ResetPassowrd";
 
+//nav bars
 import HRNav from "./Components/NavBars/hrNav";
 import CINav from "./Components/NavBars/ciNav";
 import HODNav from "./Components/NavBars/hodNav";
+import CCNav from './Components/NavBars/ccNav'
 
+//course instructor
 import viewSlotsAssignment from "./Components/Pages/Academic Members/Course Instructor/viewSlots";
 import ViewCoverage from "./Components/Pages/Academic Members/Course Instructor/viewCoverage";
 import ViewAllStaff from "./Components/Pages/Academic Members/Course Instructor/viewAllStaff";
 import ButtonsPage from "./Components/Pages/Academic Members/Course Instructor/buttonsPage";
 import coursesCI from "./Components/Pages/Academic Members/Course Instructor/coursesCI";
 
+//hod
 import CoursesListView from "./Components/List Views HOD/coursesListView";
 import InstructorsListViewPerDep from "./Components/List Views HOD/InstructorsListViewPerDep";
 import  ButtonsPageHod from "./Components/List Views HOD/buttonsPageHOD";
@@ -29,8 +36,8 @@ import ViewDayOffX from "./Components/Pages/Academic Members/HOD/viewDayOffX";
 import AcceptRejectReq from "./Components/Pages/Academic Members/HOD/acceptRejectReq";
 import ViewCoverageHOD from "./Components/Pages/Academic Members/HOD/viewCoverageHOD";
 import ViewAttendance from "./Components/Pages/Staff Members/View Attendance/ViewAttendance";
-import ViewAttendanceRecord from './Components/Pages/HR Members/viewAttendanceRecord'
 
+//academic member
 import CompensationLeave from './Components/Pages/Academic Members/Academic Member/Leave System/CompensationLeave'
 import AccidentialLeave from './Components/Pages/Academic Members/Academic Member/Leave System/AccidentialLeave'
 import AnnualLeave from './Components/Pages/Academic Members/Academic Member/Leave System/AnnualLeave'
@@ -48,52 +55,56 @@ import changeDayOff from "./Components/Pages/Academic Members/Academic Member/Ch
 import isAuth from "./Services/isAuth";
 import { SlotLinkingRequests } from "./Components/Pages/Academic Members/Course Coordinator/SlotLinkingRequests";
 import { CourseSlots } from "./Components/Pages/Academic Members/Course Coordinator/CourseSlots";
-import StaffAttendance from "./Components/Pages/HR Members/View Staff Member Attendance/StaffAttendance";
-import CCNav from './Components/NavBars/ccNav'
+
 ReactDOM.render(
   <Router>
    <CCNav/>
-    <Route path="/login" exact component={Login} />
-    <Route path="/" exact component={ViewMyProfile} />
-    <Route path="/resetPassword" exact component={ResetPassowrd} />
-    <Route path="/attendance" exact component={ViewAttendance} />
-    <Route path="/courseSlots" exact component={CourseSlots} />
-    <Route path="/slotLinkingRequests" exact component={SlotLinkingRequests} />
-    <Route path="/staffAttendance" exac component={StaffAttendance} />
-    <Route path="/login/CI/viewCoverage" exact component={ViewCoverage} />
-    <Route path="/login/CI/viewSlotsAssignment" exact component={viewSlotsAssignment} />
-    <Route path="/login/CI/viewAllStaff" exact component={ViewAllStaff} />
-    <Route path="/login/CI/buttonsPage" exact component={ButtonsPage} />
-    <Route path="/login/CI/coursesCI" exact component={coursesCI} />
+    {/*Staff Members*/}
+    <ProtectedRoute path="/login" exact component={Login} />
+    <ProtectedRoute path="/" exact component={ViewMyProfile} />
+    <ProtectedRoute path="/resetPassword" exact component={ResetPassowrd} />
+    <ProtectedRoute path="/attendance" exact component={ViewAttendance} />
 
-    <Route path="/coursesListView" exact component={CoursesListView} />
-    <Route path="/buttonsPageHod" exact component={ButtonsPageHod} />
+    {/*Course Coordinator*/}
+    <ProtectedRoute path="/courseSlots" exact component={CourseSlots} />
+    <ProtectedRoute path="/slotLinkingRequests" exact component={SlotLinkingRequests} />
   
-    <Route path="/InstructorsListViewPerDep" exact component={InstructorsListViewPerDep} />
-    <Route path="/viewAllReq" exact component={viewAllReq} />
-    <Route path="/viewTeachingAssignments" exact component={viewTeachingAssignments} />
-    <Route path="/viewDayOffAll" exact component={ViewDayOffAll} />
-    <Route path="/viewDayOffX" exact component={ViewDayOffX} />
-    <Route path="/acceptReject" exact component={AcceptRejectReq} />
-    <Route path="/viewCoverageHOD" exact component={ViewCoverageHOD} />
-    <Route path="/viewCov" exact component={ViewCoverage} />
+    {/*Course Instructor*/}
+    <ProtectedRoute path="/login/CI/viewCoverage" exact component={ViewCoverage} />
+    <ProtectedRoute path="/login/CI/viewSlotsAssignment" exact component={viewSlotsAssignment} />
+    <ProtectedRoute path="/login/CI/viewAllStaff" exact component={ViewAllStaff} />
+    <ProtectedRoute path="/login/CI/buttonsPage" exact component={ButtonsPage} />
+    <ProtectedRoute path="/login/CI/coursesCI" exact component={coursesCI} />
 
-    <Route path="/InstructorsListViewPerCourse" exact component={InstructorsListViewPerCourse} />
-    <Route path="/buttonsPageHod2" exact component={ButtonsPageHod2} />
-    <Route path="/compensationLeave" exact component={CompensationLeave} />
-    <Route path="/accidentialLeave" exact component={AccidentialLeave} />
-    <Route path="/annualLeave" exact component={AnnualLeave} />
-    <Route path="/maternityLeave" exact component={MaternityLeave} />
-    <Route path="/sickLeave" exact component={SickLeave} />
-    <Route path="/viewAll" exact component={viewAll} />
-    <Route path="/viewAccepted" exact component={viewAccepted} />
-    <Route path="/viewPending" exact component={ViewPending} />
-    <Route path="/viewRejected" exact component={viewRejected} />
-    <Route path="/sendReplacementReq" exact component={SendReplacementReq} />
-    <Route path="/viewReplacementReq" exact component={ViewReplacementReq} />
-    <Route path="/SlotLinkingReq" exact component={SlotLinkingReq} />
-    <Route path="/viewSchedule" exact component={ViewSchedule} />
-    <Route path="/changeDayOff" exact component={changeDayOff} />
+     {/*HOD*/}
+    <ProtectedRoute path="/coursesListView" exact component={CoursesListView} />
+    <ProtectedRoute path="/buttonsPageHod" exact component={ButtonsPageHod} />
+    <ProtectedRoute path="/InstructorsListViewPerDep" exact component={InstructorsListViewPerDep} />
+    <ProtectedRoute path="/viewAllReq" exact component={viewAllReq} />
+    <ProtectedRoute path="/viewTeachingAssignments" exact component={viewTeachingAssignments} />
+    <ProtectedRoute path="/viewDayOffAll" exact component={ViewDayOffAll} />
+    <ProtectedRoute path="/viewDayOffX" exact component={ViewDayOffX} />
+    <ProtectedRoute path="/acceptReject" exact component={AcceptRejectReq} />
+    <ProtectedRoute path="/viewCoverageHOD" exact component={ViewCoverageHOD} />
+    <ProtectedRoute path="/viewCov" exact component={ViewCoverage} />
+    <ProtectedRoute path="/InstructorsListViewPerCourse" exact component={InstructorsListViewPerCourse} />
+    <ProtectedRoute path="/buttonsPageHod2" exact component={ButtonsPageHod2} />
+
+     {/*Academic Members*/}
+    <ProtectedRoute path="/compensationLeave" exact component={CompensationLeave} />
+    <ProtectedRoute path="/accidentialLeave" exact component={AccidentialLeave} />
+    <ProtectedRoute path="/annualLeave" exact component={AnnualLeave} />
+    <ProtectedRoute path="/maternityLeave" exact component={MaternityLeave} />
+    <ProtectedRoute path="/sickLeave" exact component={SickLeave} />
+    <ProtectedRoute path="/viewAll" exact component={viewAll} />
+    <ProtectedRoute path="/viewAccepted" exact component={viewAccepted} />
+    <ProtectedRoute path="/viewPending" exact component={ViewPending} />
+    <ProtectedRoute path="/viewRejected" exact component={viewRejected} />
+    <ProtectedRoute path="/sendReplacementReq" exact component={SendReplacementReq} />
+    <ProtectedRoute path="/viewReplacementReq" exact component={ViewReplacementReq} />
+    <ProtectedRoute path="/SlotLinkingReq" exact component={SlotLinkingReq} />
+    <ProtectedRoute path="/viewSchedule" exact component={ViewSchedule} />
+    <ProtectedRoute path="/changeDayOff" exact component={changeDayOff} />
   </Router>,
 
   document.getElementById("root")
